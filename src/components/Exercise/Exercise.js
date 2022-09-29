ce27fe2cce7bdd506a99bc23fcdaca9d0a5954b2
MD SHIMUL <80319580+mdshimul101@users.noreply.github.com>
2022-09-29 18:40:54 +0600

    Exercise added

---
+++ src/components/Exercise/Exercise.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Exercise = ({ time, total }) => {
+    return (
+        <div className="exercise">
+            <h2>Exercise Details</h2>
+            <div className="time">
+                <p>Exercise Time: {total}min</p>
+            </div>
+            <div className="break-time">
+                <p>Break Time: {time}min</p>
+            </div>
+        </div>
+    );
+};
+
+export default Exercise;
--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,15 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Info.css'
 
 
 
 import 'react-toastify/dist/ReactToastify.css';
 import Profile from '../Profile/Profile';
+import Exercise from '../Exercise/Exercise';
 
-const Info = () => {
+const Info = ({ info }) => {
+    const [time, setTime] = useState(0);
+
+    let total = 0;
+
+    for (const activity of info) {
+
+
+        total = parseInt(total + activity.timeRequired);
+
+    }
+
+    const timeSet = (time) => {
+
+
+        setTime(time);
+        localStorage.setItem('time', JSON.stringify(time));
+
+
+    }
+    useEffect(() => {
+
+        const preTime = localStorage.getItem('time');
+        if (preTime) {
+            setTime(preTime);
+        }
+
+
+    }, [time])
     return (
         <div className="info-container">
             <Profile></Profile>
+            <div className="break">
+                <h2>Add A Break</h2>
+                <div>
+                    <button onClick={() => timeSet(10)} className='btn1'>10min</button>
+                    <button onClick={() => timeSet(30)} className='btn2'>30min</button>
+                    <button onClick={() => timeSet(45)} className='btn3'>45min</button>
+                    <button onClick={() => timeSet(60)} className='btn4'>60min</button>
+
+                </div>
+            </div>
+            <Exercise total={total} time={time}></Exercise>
         </div>
     );
 };
