bb7f92febc7932b23c7d1215255acc0d6499df11
MD SHIMUL <80319580+mdshimul101@users.noreply.github.com>
2022-09-29 21:06:46 +0600

    FAQ Change

--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -9,7 +9,7 @@ const Faq = () => {
                 <h2>1.How does react works?</h2>
                 <p>ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code. </p>
                 <h2>2.What is the difference between props and state?<br /></h2>
-                <p>The key difference between props and state is that state is internal and controlled by the component itself while props are external and controlled by whatever renders the component.</p>
+                <p>Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components. Props are read-only.State is both read and write. Props is Immutable (cannot be modified). State is Mutable ( can be modified).</p>
                 <h2>3.What does useEffect do without data load?</h2>
                 <p>The useEffect Hook allows you to perform side effects in your components. Some examples of side effects are
                     directly updating  the DOM, and timers. useEffect accepts two arguments. The second argument is optional.</p>
